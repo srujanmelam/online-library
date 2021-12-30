ad22ed287c67206cfbe1873bd84bfc9f4acc1320
Manoj-31 <saimanojkumar.p19@iiits.in>
2021-12-30 15:12:10 +0530

    .

--- src/Component/SignIn.js
+++ src/Component/SignIn.js
@@ -2,12 +2,24 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import store from "./store";
+import { Button, InputAdornment, styled, TextField } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import { AccountCircle, LockRounded} from "@material-ui/icons" 
+
 
 function SignIn() {
   const [username1, setUsername] = useState("");
   const [password1, setPassword] = useState("");
   const [message, setMessage] = useState("");
   const history = useNavigate();
+
+  const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));  
 
   const LoginUser = ()=>{
     axios.get(`http://localhost:3000/users?username=${username1}&password=${password1}`).then( res =>{
@@ -30,15 +42,74 @@ function SignIn() {
 
   return (
     <div>
-      <h2>SignIn</h2>
-      <input type="text" placeholder="Enter Username" onChange={(e) => setUsername(e.target.value)} required/>
-      <input type="password" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} required/>
-      <input type="checkbox" value="lsRememberMe"/>
-      <label htmlFor="rememberMe">Remember me</label>
-      <p>{message}</p>
-      <button type="submit" onClick={LoginUser}>Login</button>
-      <h6>Don't have an account?</h6>
-      <Link to="/signup">Sign Up</Link>
+      <Grid container spacing={1}>
+        <Grid item xs={12} md={6} lg={6}>
+          <img src="https://st2.depositphotos.com/1177973/6352/i/950/depositphotos_63529855-stock-photo-e-learning-concept-digital-library.jpg" 
+            style={{width: '100%', height: '100%', objectfits:'cover'}} 
+            alt='brand'
+          />  
+        </Grid>
+        <Grid container item 
+          xs={12} 
+          md={6} 
+          lg={6}
+          alignItems="center"
+          direction="column"
+          justify="space-between"
+          style={{ padding: 10 }}
+        >
+          <div />
+          < div 
+            style={{ 
+              display: "flex", 
+              flexDirection: "column",
+              maxWidth: 400,
+              minWidth: 300,
+            }}
+          >
+            <Grid container justify="center">
+              <img src= "https://cdn.dribbble.com/users/5707100/screenshots/15403508/media/36df88a25ddf58eab0dda29ded1ac920.png?compress=1&resize=400x300"
+                width={200}
+                alt='logo'
+              />
+              </Grid>
+              <TextField
+                label="Username" 
+                margin="normal"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <AccountCircle />
+                    </InputAdornment> 
+                  ),
+                }}
+              >
+              </TextField>
+              <TextField 
+                label="Password" 
+                margin="normal"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <LockRounded />
+                    </InputAdornment>  
+                  ),
+                }}
+              >
+              </TextField>
+              <div style={{ height: 20}}>
+                <Button color="primary" variant="contained">
+                  Sign In
+                </Button>
+              </div>
+              <div>
+                <Button>Don't Have An Account?</Button>
+              </div>
+          </div>
+          <div />
+        </Grid >
+      </Grid>
+      
     </div>
   );
 }
