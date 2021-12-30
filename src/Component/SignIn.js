c39c481c5807fa91e25304ee09330fb52fa37cdb
Manoj-31 <saimanojkumar.p19@iiits.in>
2021-12-30 15:25:10 +0530

    material design for signin and signup pages

--- src/Component/SignIn.js
+++ src/Component/SignIn.js
@@ -2,120 +2,169 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import store from "./store";
-import { Button, InputAdornment, styled, TextField } from '@material-ui/core';
-import { Paper } from '@material-ui/core';
-import { Grid } from '@material-ui/core';
-import { AccountCircle, LockRounded} from "@material-ui/icons" 
+import {
+  styled,
+  Paper,
+  Grid,
+  TextField,
+  Button,
+  InputAdornment,
+  Checkbox,
+  Typography,
+} from "@material-ui/core";
 
-
+import { AccountCircle, LockRounded } from "@material-ui/icons";
 function SignIn() {
   const [username1, setUsername] = useState("");
   const [password1, setPassword] = useState("");
   const [message, setMessage] = useState("");
   const history = useNavigate();
 
-  const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));  
-
-  const LoginUser = ()=>{
-    axios.get(`http://localhost:3000/users?username=${username1}&password=${password1}`).then( res =>{
-      if (res.data.length!==0){
-        const user = {
-          "username" : res.data[0].username,
-          "isAdmin" : res.data[0].isAdmin
+  const LoginUser = () => {
+    axios
+      .get(
+        `http://localhost:3000/users?username=${username1}&password=${password1}`
+      )
+      .then((res) => {
+        if (res.data.length !== 0) {
+          store.dispatch({
+            type: "loginSuccess",
+            payload: res.data[0].username,
+          });
+          console.log("Login Success");
+          history("/home");
+        } else {
+          store.dispatch({ type: "loginFail" });
+          console.log("Invalid Username or Password");
+          setMessage("Username or Password is Invalid");
         }
-        store.dispatch({type:"loginSuccess",payload:user}); 
-        console.log("Login Success")  
-        history("/home");
-      }  
-      else{
-        store.dispatch({type:"loginFail"});
-        console.log("Invalid Username or Password");
-        setMessage("Username or Password is Invalid");
-      }
-   }).catch((err)=>{
-       store.dispatch({type:"loginFail"});
-       setMessage(err);
-       console.log(err);
-   })
-  }
-
+      })
+      .catch((err) => {
+        store.dispatch({ type: "loginFail" });
+        setMessage(err);
+        console.log(err);
+      });
+  };
+ 
   return (
     <div>
-      <Grid container spacing={1}>
-        <Grid item xs={12} md={6} lg={6}>
-          <img src="https://st2.depositphotos.com/1177973/6352/i/950/depositphotos_63529855-stock-photo-e-learning-concept-digital-library.jpg" 
-            style={{width: '100%', height: '100%', objectfits:'cover'}} 
-            alt='brand'
-          />  
+      <Grid
+        container
+        minWidth="100vh"
+        style={{ border: "solid", minWidth: "100%", height: "100vh" }}
+      >
+
+      
+        <Grid item xs={12} md={7} lg={7}>
+          <img
+            src="https://c4.wallpaperflare.com/wallpaper/417/445/320/anime-original-book-chair-library-hd-wallpaper-preview.jpg"
+            style={{ width: "100%", height: "100%", objectfits: "cover"}}
+            
+            alt="brand"
+          />
         </Grid>
-        <Grid container item 
-          xs={12} 
-          md={6} 
-          lg={6}
+        <Grid
+          
+          container
+          item
+          
+          xs={12}
+          md={5}
+          lg={5}
           alignItems="center"
           direction="column"
           justify="space-between"
-          style={{ padding: 10 }}
+          style={{ color:"white",padding: 10 ,backgroundImage: `url('https://images.unsplash.com/photo-1615799998586-54a1591888bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJvd24lMjBwYXBlcnxlbnwwfHwwfHw%3D&w=1000&q=80')`}}
         >
           <div />
-          < div 
-            style={{ 
-              display: "flex", 
+          <div />
+          <div
+            style={{
+              display: "flex",
               flexDirection: "column",
               maxWidth: 400,
               minWidth: 300,
             }}
           >
-            <Grid container justify="center">
-              <img src= "https://cdn.dribbble.com/users/5707100/screenshots/15403508/media/36df88a25ddf58eab0dda29ded1ac920.png?compress=1&resize=400x300"
+            <Grid container justify="center"
+          >
+              <img
+                src="https://cdn5.vectorstock.com/i/thumb-large/75/49/panda-book-read-newspaper-negative-space-logo-icon-vector-39827549.jpg"
                 width={200}
-                alt='logo'
+                alt="logo" 
+                style={{borderRadius:"50%"}}
               />
+            </Grid>
+            <TextField
+              type="text"
+              label="Username"
+              margin="normal"
+              color="white"
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment>
+                    <AccountCircle />
+                  </InputAdornment>
+                ),
+              }}
+            ></TextField>
+
+            <TextField
+              type="password"
+              label="Password"
+              margin="normal"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment>
+                    <LockRounded />
+                  </InputAdornment>
+                ),
+              }}
+            ></TextField>
+            <Grid container justify="center" spacing={2}>
+              <Grid item>
+                <Typography htmlFor="rememberMe">Remember me</Typography>
               </Grid>
-              <TextField
-                label="Username" 
-                margin="normal"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <AccountCircle />
-                    </InputAdornment> 
-                  ),
-                }}
-              >
-              </TextField>
-              <TextField 
-                label="Password" 
-                margin="normal"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <LockRounded />
-                    </InputAdornment>  
-                  ),
-                }}
-              >
-              </TextField>
-              <div style={{ height: 20}}>
-                <Button color="primary" variant="contained">
-                  Sign In
-                </Button>
-              </div>
-              <div>
-                <Button>Don't Have An Account?</Button>
-              </div>
+              <Grid item>
+                <Checkbox
+                  type="checkbox"
+                  color="brown"
+                  value="lsRememberMe"
+                ></Checkbox>
+              </Grid>
+            </Grid>
+            <Typography>{message}</Typography>
+            <div style={{ height: 20 }} />
+            <Button
+              type="submit"
+              onClick={LoginUser}
+              color="4E342E"
+              variant="contained"
+            >
+              Sign In
+            </Button>
+            <div style={{ height: 20 }} />
+            <Grid container justify="center" spacing={2}>
+              <Grid item>
+                <Typography>Don't have an account?</Typography>
+              </Grid>
+              <Grid item>
+               
+                  <Link to="/signup" style={{textDecoration:"none",color:"black"}}>Sign Up</Link>
+              </Grid>
+            </Grid>
           </div>
+
           <div />
-        </Grid >
+        </Grid>
       </Grid>
-      
     </div>
   );
 }
 
 export default SignIn;
+
--- src/Component/SignUp.js
+++ src/Component/SignUp.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
+import {
+  Grid,
+  TextField,
+  Button,
+  InputAdornment,
+  Checkbox,
+  Typography,
+} from "@material-ui/core";
 
+import { AccountCircle, LockRounded } from "@material-ui/icons";
 function SignUp() {
   const [username1, setUsername] = useState("");
   const [password1, setPassword] = useState("");
@@ -10,49 +19,150 @@ function SignUp() {
 
   const history = useNavigate();
 
-  const SignUpUser = ()=>{
+  const SignUpUser = () => {
     const user = {
-      "username":username1,
-      "password":password1,
-      "isAdmin":isAdmin1
-    }
-    if (username1.length > 0 & password1.length > 0){
-      axios.post(`http://localhost:3000/users`,user).then((res)=>{
-        console.log("user created successfully")
-        history("/")
-      }).catch((err)=>{
-        setMessage(err)
-        console.log(err);
-      })
-    }
-    else{
-      if(username1.length === 0)
-      {
-        console.log("username should not be empty")
-        setMessage("Username should not be empty")
+      username: username1,
+      password: password1,
+      isAdmin: isAdmin1,
+    };
+    if ((username1.length > 0) & (password1.length > 0)) {
+      axios
+        .post(`http://localhost:3000/users`, user)
+        .then((res) => {
+          console.log("user created successfully");
+          history("/");
+        })
+        .catch((err) => {
+          setMessage(err);
+          console.log(err);
+        });
+    } else {
+      if (username1.length === 0) {
+        console.log("username should not be empty");
+        setMessage("Username should not be empty");
       }
-      if(password1.length === 0)
-      {
-        console.log("password should not be empty")
-        setMessage("Password should not be empty")
+      if (password1.length === 0) {
+        console.log("password should not be empty");
+        setMessage("Password should not be empty");
       }
     }
-  }
+  };
 
-  return(
+  return (
     <div>
-      <h2>SignUp</h2>
-      <input type="text" placeholder="Enter Username" onChange={(e)=>setUsername(e.target.value)} required />
-      <input type="password" placeholder="Enter Password" onChange={(e)=>setPassword(e.target.value)} required />
-	    <label>Admin</label>
-      <input name="isAdmin" type="checkbox" checked={isAdmin1} onChange={(e)=>setisAdmin(!isAdmin1)} />
-      <p>{message}</p>
-	    <button type="submit" onClick={SignUpUser}>Sign Up</button>
-      <h6>Already have an account?</h6>
-      <Link to="/">Sign In</Link>
+      <Grid
+        container
+        style={{
+          border: "solid",
+          minWidth: "100%",
+          height: "100vh",
+        }}
+      >
+        <Grid item xs={12} md={7} lg={7}>
+          <img
+            src="https://c1.wallpaperflare.com/preview/542/360/877/library-books-montessori-school.jpg"
+            style={{ width: "100%", height: "100%", objectfits: "cover" }}
+            alt="brand"
+          />
+        </Grid>
+        <Grid
+          container
+          item
+          xs={12}
+          md={5}
+          lg={5}
+          alignItems="center"
+          direction="column"
+          justify="space-between"
+          style={{ padding: 10 }}
+        >
+          <div />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              maxWidth: 400,
+              minWidth: 300,
+            }}
+          >
+            <Grid container justify="center">
+              <img
+                src="https://cdn5.vectorstock.com/i/thumb-large/75/49/panda-book-read-newspaper-negative-space-logo-icon-vector-39827549.jpg"
+                width={200}
+                alt="logo"
+              />
+            </Grid>
+            <TextField
+              type="text"
+              label="Username"
+              margin="normal"
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment>
+                    <AccountCircle />
+                  </InputAdornment>
+                ),
+              }}
+            ></TextField>
+            <TextField
+              type="password"
+              label="Password"
+              margin="normal"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment>
+                    <LockRounded />
+                  </InputAdornment>
+                ),
+              }}
+            ></TextField>
+            <Grid container justify="center" spacing={2}>
+              <Grid item>
+                <Typography>Admin</Typography>
+              </Grid>
+              <Grid item>
+                <Checkbox
+                  name="isAdmin"
+                  type="checkbox"
+                  checked={isAdmin1}
+                  color="primary"
+                  onChange={(e) => setisAdmin(!isAdmin1)}
+                ></Checkbox>
+              </Grid>
+            </Grid>
+            <Typography>{message}</Typography>
+            <div style={{ height: 20 }} />
+            <Button
+              type="submit"
+              onClick={SignUpUser}
+              color="primary"
+              variant="contained"
+            >
+              SignUp
+            </Button>
+
+            <div style={{ height: 20 }} />
+            <Grid container justify="center" spacing={2}>
+              <Grid item>
+                <Typography>Already have an account?</Typography>
+              </Grid>
+              <Grid item>
+                <Button variant="outlined">
+                  <Link to="/">Sign In</Link>
+                </Button>
+              </Grid>
+            </Grid>
+          </div>
+
+          <div />
+        </Grid>
+      </Grid>
     </div>
   );
-
 }
 
-export default SignUp
+export default SignUp;
