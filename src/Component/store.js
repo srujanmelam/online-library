bc0bc6b74523302a060e461c6d8c0a5c1b4d081e
manoj-b-22 <manojsrinivas.b19@iiits.in>
2022-01-07 13:27:21 +0530

    fixed reload state error

--- src/Component/store.js
+++ src/Component/store.js
@@ -1,8 +1,11 @@
 import rootReducer from "./reducers/rootReducer";
 import { createStore } from "redux";
 
+const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {}
+
 const store = createStore(
   rootReducer,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
--- src/index.js
+++ src/index.js
@@ -15,6 +15,10 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+store.subscribe(()=>{
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+})
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
